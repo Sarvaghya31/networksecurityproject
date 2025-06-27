fa65d38daa7a8b470fb6704bba231d7328890e35
Sarvaghya31 <sarvaghyajoshi2003@gmail.com>
2025-06-27 12:24:02 +0530

    update

--- push_data.py
+++ push_data.py
@@ -23,8 +23,8 @@ class NetworkDataExtract():
             pass
         except Exception as e:
             raise NetworkSecurityException(e,sys)
-        
-    def csv_to_json_convertor(self,file_path):
+    #this pipeline seems like a EL pipeline    
+    def csv_to_json_convertor(self,file_path):    #this conversion to json is considered as tranform here
         try:
             data=pd.read_csv(file_path)
             data.reset_index(drop=True,inplace=True)
@@ -50,8 +50,8 @@ class NetworkDataExtract():
         
 if __name__=='__main__':
     FILE_PATH="Network_Data\phisingData.csv"
-    DATABASE="NetworkSecurity"
-    Collection="NetworkData"
+    DATABASE="networksecurity"
+    Collection="networkData"
     networkobj=NetworkDataExtract()
     records=networkobj.csv_to_json_convertor(file_path=FILE_PATH)
     print(records)
